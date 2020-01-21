2ef2ca6973c5472bd69c8fab3c1789784d92847b
Bobbyleo <41812240+BobbyBriQz@users.noreply.github.com>
2020-01-21 11:35:19 +0100

    UI changes in LoginActivity and RegisterActivity.

--- app/src/main/java/com/appsbygreatness/ideasappformobiledevelopers/activities/RegisterActivity.java
+++ app/src/main/java/com/appsbygreatness/ideasappformobiledevelopers/activities/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.appsbygreatness.ideasappformobiledevelopers.activities;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +21,9 @@ public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.DarkTheme);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
